e63eec51404fb4c40de597dad887084a39c0cf83
Hasan Wirayuda <hasanwirayuda@Hasans-MacBook-Air.local>
2024-03-21 16:42:49 +0700

    menambahkan halaman home

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from "./gamma-force-shoes-MFG9vD.png";
+import p2_img from "./dunk-low-shoes-3wjGFP.png";
+import p3_img from "./air-more-uptempo-shoes-mcpGmG.png";
+import p4_img from "./dunk-low-shoes-dtD4vl.png";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Nike Gamma Force",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Nike Dunk Low",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Nike Air More Uptempo",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Nike Dunk Low",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default data_product;
---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./collections1.png";
+import p2_img from "./collections2.png";
+import p3_img from "./collections3.png";
+import p4_img from "./collections4.png";
+import p5_img from "./collections5.png";
+import p6_img from "./collections6.png";
+import p7_img from "./collections7.png";
+import p8_img from "./collections8.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Nike Air Force 1 '07 LV8",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "Nike Air Max 1 SE",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Nike Dunk Low Retro",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "Nike P-6000 Premium",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 15,
+    name: "Nike Air Force 1 '07",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Nike Air Dunk Low Jumbo",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Nike Air Max 1",
+    image: p7_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 28,
+    name: "Nike Court Vision Low",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default new_collections;
