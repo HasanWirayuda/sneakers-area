b53c8ab7537cd13cfb3c12b79dbc0d3aadd22e06
Hasan Wirayuda <hasanwirayuda@Hasans-MacBook-Air.local>
2024-04-23 22:56:46 +0700

    menambahkan detail produk

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             element={<ShopCategory banner={kid_banner} category="kid" />}
           />
           <Route path="/product" element={<Product />}>
-            <Route path=":productID" element={<Product />} />
+            <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
