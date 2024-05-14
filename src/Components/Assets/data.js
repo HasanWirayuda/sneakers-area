610f033b4fb2357d6f0e38376b8b8656cebfd94a
Hasan Wirayuda <hasanwirayuda@Hasans-MacBook-Air.local>
2024-05-14 22:47:59 +0700

    menambahkan related produk dan keranjang belanja

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,33 +1,33 @@
-import p1_img from "./gamma-force-shoes-MFG9vD.png";
-import p2_img from "./dunk-low-shoes-3wjGFP.png";
-import p3_img from "./air-more-uptempo-shoes-mcpGmG.png";
-import p4_img from "./dunk-low-shoes-dtD4vl.png";
+import p1_img from "./women12.png";
+import p2_img from "./women2.png";
+import p3_img from "./women3.png";
+import p4_img from "./women4.png";
 
 let data_product = [
   {
     id: 1,
-    name: "Nike Gamma Force",
+    name: "Nike Dunk Low Next Nature",
     image: p1_img,
     new_price: 50.0,
     old_price: 80.5,
   },
   {
     id: 2,
-    name: "Nike Dunk Low",
+    name: "Nike SB Dunk Low Premium",
     image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
   },
   {
     id: 3,
-    name: "Nike Air More Uptempo",
+    name: "Nike Cortez",
     image: p3_img,
     new_price: 60.0,
     old_price: 100.5,
   },
   {
     id: 4,
-    name: "Nike Dunk Low",
+    name: "Nike Zoom Vomero 5 SE",
     image: p4_img,
     new_price: 100.0,
     old_price: 150.0,
